30e8409cc5ecdbec6cd5410db0dbfaea45f53250
Samantha Hughes <samantha.patricia.hughes2@gmail.com>
2018-07-16 17:43:19 -0400

    first dev commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Parallax, ParallaxLayer } from 'react-spring'
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       My Portfolio
+      </div>
+    );
+  }
+}
+
+export default App;
